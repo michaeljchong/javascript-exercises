8434ca7fd777b8995ff9676d45add6b2d52c451b
Michael Chong <mikec0920@gmail.com>
2021-08-25 23:29:26 -0700

    Completed sum range of numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof(num1) !== "number" || typeof(num2) !== "number" ||
+      num1 < 0 || num2 < 0) return "ERROR";
 
+  let sum = 0;
+  let startNumber = (num1 <= num2) ? num1 : num2;
+  let endNumber = (startNumber === num1) ? num2 : num1; 
+  
+  while (startNumber <= endNumber) {
+    sum += startNumber;
+    startNumber++;
+  }
+
+  return sum;
 };
 
 module.exports = sumAll;
