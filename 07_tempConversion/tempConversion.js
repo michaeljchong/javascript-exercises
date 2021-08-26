bdc1e079cfed8510396d61f02f23c02b00fb7050
Michael Chong <mikec0920@gmail.com>
2021-08-26 00:03:19 -0700

    Fixed issue involving order of operations

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(temp) {
   // c = (f-32) * 5/9
-  return Math.round((temp - 32) * 5 / 9 * 10) / 10;
+  return Math.round(((temp - 32) * 5 / 9) * 10) / 10;
 };
 
 const ctof = function(temp) {
   // f = 9/5 * c + 32
-  return Math.round(9 / 5 * temp + 32 * 10) / 10;
+  return Math.round((9 / 5 * temp + 32) * 10) / 10;
 };
 
 module.exports = {
