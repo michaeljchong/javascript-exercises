0b77d6d7c16c30d9eb5836c4a18148000f49dbc2
Michael Chong <mikec0920@gmail.com>
2021-08-25 20:34:12 -0700

    Passed first test case

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let newString = "";
+  for (i=0; i < num; i++) newString += str;
+  return newString;
 };
 
 module.exports = repeatString;
