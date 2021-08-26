28a6285a92795f2515aa7aa8c49852dda72d55e3
Michael Chong <mikec0920@gmail.com>
2021-08-26 00:19:27 -0700

    Refactor logic to one-line

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,5 @@
 const leapYears = function(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) return true;
-      return false;
-    }
-    return true;
-  }
-  return false;
-};
+  return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0);
+}
 
 module.exports = leapYears;
