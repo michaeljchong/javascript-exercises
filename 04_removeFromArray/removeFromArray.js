55edd81df8504c9d8d3535023a6403b2012730ed
Michael Chong <mikec0920@gmail.com>
2021-08-25 22:09:41 -0700

    Completed remove from array function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  for (i=0; i < args.length; i++) {
+    while (arr.includes(args[i])) {
+      arr.splice(arr.indexOf(args[i]),1)
+    }
+  }
+  return arr;
 };
 
 module.exports = removeFromArray;
