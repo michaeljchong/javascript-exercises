497dcd082eb9e7b55e6bd2b5d660944a715f4b1f
Michael Chong <mikec0920@gmail.com>
2021-09-04 10:26:51 -0700

    Completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  alpha = /[a-z]/i;
+  const lowerAlpha = phrase
+                    .split('')
+                    .filter(char => alpha.test(char))
+                    .map(char => char.toLowerCase());
+  
+  let firstHalf = lowerAlpha.slice(0, lowerAlpha.length / 2);
+  let secondHalf = lowerAlpha.slice(lowerAlpha.length / 2);
+  if (lowerAlpha.length % 2 === 1) {
+    firstHalf = lowerAlpha.slice(0, lowerAlpha.length / 2);
+    secondHalf = lowerAlpha.slice(lowerAlpha.length / 2 + 1);
+  }
+  
+  return firstHalf.join('') === secondHalf.reverse().join('');
 };
 
 module.exports = palindromes;
+
+/*
+remove non alphabetical characters and whitespace
+change all characters to lowercase
+if length is even, split in half
+if length is odd, split leaving the middle character
+reverse second half
+compare first and second halves
+*/
