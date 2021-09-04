82c02eaac09d81449f6bd600a5f02e8705198042
Michael Chong <mikec0920@gmail.com>
2021-09-03 23:20:44 -0700

    exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let product = 1;
+  for (let i = 0; i < nums.length; i++) {
+    product *= nums[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
 };
 
 module.exports = {
